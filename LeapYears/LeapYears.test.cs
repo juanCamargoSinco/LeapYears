cd75857b589750b1b1be7fdd30a8b2763b038f12
Juan Manuel Camargo Segura <juan.camargo@sinco.co>
2025-04-07 11:07:24 -0500

    🟥 Debe_devolver_false_si_el_año_no_es_biciesto

--- LeapYears/LeapYears.test.cs
+++ LeapYears/LeapYears.test.cs
@@ -1,16 +1,24 @@
 
+
 namespace LeapYears
 {
     public class LeapYearsTest
     {
         [Fact]
-        public void StartUp()
+        public void Debe_devolver_false_si_el_año_no_es_biciesto()
         {
-
+            bool añoBiciesto = ProcesadorAños.ValidarAñoBiciesto(1993);
+            Assert.False(añoBiciesto);
         }
 
 
     }
 
-
+    internal class ProcesadorAños
+    {
+        internal static bool ValidarAñoBiciesto(int año)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
